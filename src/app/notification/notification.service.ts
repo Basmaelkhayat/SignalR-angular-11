4ec7643d3643eefd7421a7bf2054a59363c4e455
Basma elkhayat <b.khayat@myzeal.app>
2022-01-27 15:39:28 +0200

    create signal-r application example

---
+++ src/app/notification/notification.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { NotificationCountResult, NotificationResult } from './notification';
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotificationService {
+  private notificationsUrl = environment.baseUrl + 'api/--';
+
+  constructor(private http: HttpClient) {}
+
+  getNotificationCount(): Observable<NotificationCountResult> {
+    const url = `${this.notificationsUrl}/--`;
+    return this.http
+      .get<NotificationCountResult>(url)
+      .pipe(catchError(this.handleError));
+  }
+
+  getNotificationMessage(): Observable<Array<NotificationResult>> {
+    const url = `${this.notificationsUrl}/--`;
+    return this.http
+      .get<Array<NotificationResult>>(url)
+      .pipe(catchError(this.handleError));
+  }
+
+  deleteNotifications(): Observable<{}> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const url = `${this.notificationsUrl}/--`;
+    return this.http
+      .delete(url, { headers: headers })
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(err: any) {
+    let errorMessage: string;
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      errorMessage = `Backend returned code ${err.status}: ${err.body.error}`;
+    }
+    console.error(err);
+    return throwError(errorMessage);
+  }
+}
